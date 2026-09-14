4d5fde8e5490d6eaee01eef1bdb494416d7982a7
Francis Hampton <fraham5822@students.ecpi.edu>
2026-09-13 20:10:24 -0400

    Part 1 of week 2 debugging

--- MAUI_QuestBoard/AppShell.xaml.cs
+++ MAUI_QuestBoard/AppShell.xaml.cs
@@ -12,6 +12,11 @@ namespace MAUI_QuestBoard
             Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
             Routing.RegisterRoute(nameof(AddUserPage), typeof(AddUserPage));
             Routing.RegisterRoute(nameof(QuestBoardPage), typeof(QuestBoardPage));
+            Routing.RegisterRoute(nameof(AddEventPage), typeof(AddEventPage));
+            Routing.RegisterRoute(nameof(EventDetailsPage), typeof(EventDetailsPage));
+            Routing.RegisterRoute(nameof(MyAdventuresPage), typeof(MyAdventuresPage));
+            Routing.RegisterRoute(nameof(MyQuestsPage), typeof(MyQuestsPage));
+            Routing.RegisterRoute(nameof(RSVPPage), typeof(RSVPPage));
         }
     }
 }
